1d5095a2e7f5be04cd440df39831ba5f3447e10f
Md Mamun Mia <mamunahammedco59@gmail.com>
2023-01-25 01:54:53 +0600

    contact form work

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,53 @@
     jQuery(document).ready(function ($) {
 
 
+        //--------------- Email JS ---------------
+
+        const contactForm = document.getElementById('contact-form'),
+            contactName = document.getElementById('contact-name'),
+            contactEmail = document.getElementById('contact-email'),
+            contactProject = document.getElementById('contact-project'),
+            contactMessage = document.getElementById('contact-message')
+
+        const sendEmail = (e) => {
+            e.preventDefault()
+            // check if the field has value
+            if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+                // Add and remove color
+                contactMessage.classList.remove('color-green')
+                contactMessage.classList.add('color-red')
+
+                // show message
+                contactMessage.textContent = 'Write all the input fields 📩'
+            } else {
+                // serviceID - templateID - #form - publicKey
+                emailjs.sendForm('service_pi06ngc', 'template_v6k4yqn', '#contact-form', 'kQvgZRHdWgo5ybJBA')
+                    .then(() => {
+                        // Show message and add color
+                        contactMessage.classList.add('color-green')
+                        contactMessage.textContent = 'Message sent ✅'
+
+                        // Remove message after five seconds
+
+                        setTimeout(() => {
+                            contactMessage.textContent = ''
+                        }, 3000)
+
+                    }, (error) => {
+                        alert('OOPS! SOMETHING HAS FAILED...', error)
+                    })
+                // To clear the input field
+                contactName.value = ''
+                contactEmail.value = ''
+                contactProject.value = ''
+            }
+
+        }
+        contactForm.addEventListener('submit', sendEmail)
+
+
+
+
         //------------ Offcanvas -------------
 
         $('.menu-open , .offcanvas-overlay').click(function () {
